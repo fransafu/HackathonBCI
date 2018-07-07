247975e2003eb2210e6d6eb30d1131c84fd90391
juiko <juiko@debian.org.debian>
2018-07-07 19:15:29 -0400

    inicio vista registros

--- project/app.js
+++ project/app.js
@@ -12,6 +12,7 @@ import models from './models';
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var metricsRouter = require('./routes/metrics');
+var registrosRouter = require('./routes/registros');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/metrics', metricsRouter);
+app.use('/registros', registrosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
