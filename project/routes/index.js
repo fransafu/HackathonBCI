4057d512e0e7ad25d0fbbdc482218e4eeca06ae6
Francisco Javier Sánchez Fuentes <fransafu@gmail.com>
2018-07-08 11:40:55 -0400

    Cambio menor

--- project/routes/index.js
+++ project/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+var Registro = db.registro;
+var User = db.user;
+var Empresa = db.empresa;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
 });
 
