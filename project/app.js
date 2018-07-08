9d242689bdb7e144b9322324cc52831de15da99c
Francisco Javier Sánchez Fuentes <fransafu@gmail.com>
2018-07-08 13:24:43 -0400

    Modificaciones menore

--- project/app.js
+++ project/app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Definir rutas
-app.use('/', indexRouter);
+app.use('/', metricsRouter);
 app.use('/users', usersRouter);
 app.use('/metrics', metricsRouter);
 app.use('/registros', registrosRouter);
