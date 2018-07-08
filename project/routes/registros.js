8175bfb083089ecc29907a62af6fa33d2920c360
juiko <hofm92@gmail.com>
2018-07-08 00:02:12 -0400

    esqueleto crud registros

--- project/routes/registros.js
+++ project/routes/registros.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// CREATE
+router.post('/', function(req, res) {
+});
+
+// READ
 router.get('/', function(req, res, next) {
   res.render('registros', { registros: [] });
 });
+
+router.get('/:id', function(req, res, next) {
+});
+
+
+// UPDATE
+router.put('/:id', function(req, res) {
+});
+
+// DELETE
+router.delete('/:id', function(req, res) {
+});
+
 
 module.exports = router;
